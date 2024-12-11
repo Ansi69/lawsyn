686daccd1d89c96782a65b9543bb0ccce5e24159
Your Name <you@example.com>
2024-12-11 22:37:17 +0700

    gg

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from main.models import ychebniki, categories, User, Help
+from main.models import ychebniki, categories, User
 
 admin.site.register(ychebniki)
 admin.site.register(categories)
 admin.site.register(User)
-admin.site.register(Help)
